3f623d0c4419cc0ce3196820a1742a1377e8f1fb
Maurifc <mauri_vrb@hotmail.com>
2021-11-18 21:57:10 -0300

    ajustada a configuracao do nfs server para permitir qualquer host

--- Vagrant/nfs_server/setup_nfs.sh
+++ Vagrant/nfs_server/setup_nfs.sh
@@ -6,7 +6,7 @@ apt update && \
 mkdir /nfs_share
 chmod 777 /nfs_share
 
-echo "/nfs_share  192.168.0.0/16 (rw,sync,no_subtree_check)" >> /etc/exports
+echo "/nfs_share  *(rw,sync,no_subtree_check)" >> /etc/exports
 
 exportfs -a &> /dev/null
 systemctl restart nfs-kernel-server
